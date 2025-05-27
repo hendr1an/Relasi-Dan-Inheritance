36b90c588a3782a0fba0a66886c21ad0b60b08af
hendr1an <hendriansyahgyan@gmail.com>
2025-05-27 10:54:24 +0700

    Mengimplementasikan relasi dua arah antara dokter dan pasien serta menambahkan logika utama pada main

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -38,3 +38,49 @@ class dokter
     void cetakPasien();
 };
 
+void pasien::tambahDokter(dokter *pDokter)
+{
+    daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter()
+{
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto &a : daftar_dokter)
+    {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+void dokter::tambahPasien(pasien *pPasien)
+{
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetakPasien()
+{
+    cout << "Daftar pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto &a : daftar_pasien)
+    {
+        cout << a->nama << nama << "\n";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    dokter *varDokter1 = new dokter("dr.budi");
+    dokter *varDokter2 = new dokter("dr.tomo");
+    pasien *varPasien1 = new pasien("andi");
+    pasien *varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+}
